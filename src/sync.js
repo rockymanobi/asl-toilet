03b8c5936af72b3d63f54c5a7137d8041c23dafb
rockymanobi <tak.koroki@gmail.com>
2014-04-30 11:20:43 +0900

    srcディレクトリ追加

---
+++ src/sync.js
@@ -0,0 +1,32 @@
+http = function(){
+  var wlan = require("CC3000").connect();
+  var connectedToWlan = wlan.connect( "AccessPointName", "WPA2key", function (s) { 
+    if (s=="dhcp") {
+      heatBeat();
+      sync();
+    }
+  });
+
+  if(!connected){
+    console.log("ngngngng");
+
+  }
+}
+
+
+heatBeat = function(s){
+  console.log("heatBeat");
+}
+
+
+sync = function(s){
+  console.log("hoge");
+  return;
+  require("http").get("http://www.pur3.co.uk/hello.txt", function(res) {
+    res.on('data', function(data) {
+      console.log(">" + data);
+    });
+  });
+}
+
+
